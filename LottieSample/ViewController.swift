2a43c73cfe21b1c8d12006a5be84474a8cab2565
Togami Yuki <togaminnnn@gmail.com>
2019-01-15 09:49:50 +0800

    firstCommit

---
+++ LottieSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieSample
+//
+//  Created by Togami Yuki on 2019/01/15.
+//  Copyright © 2019 Togami Yuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
