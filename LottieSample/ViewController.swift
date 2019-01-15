d32c895bb74b137cfc6e6df2f1bf8b1f6014d927
Togami Yuki <togaminnnn@gmail.com>
2019-01-15 10:17:13 +0800

    アニメーション動作確認

--- LottieSample/ViewController.swift
+++ LottieSample/ViewController.swift
@@ -7,13 +7,47 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
+    
+    //アニメーションを表示するViewのインスタンス化
+    let animationView = LOTAnimationView()
+    
+    @IBOutlet weak var startBtn: UIButton!
+    @IBOutlet weak var stopBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        animationView.frame.size.width = 300//アニメーションを表示するViewの幅
+        animationView.frame.size.height = 300//アニメーションを表示するViewの高さ
+        animationView.center = self.view.center//Viewのs中心に配置
+        animationView.setAnimation(named: "gears")
+        animationView.animationSpeed = 1//アニメーションのスピード
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopAnimation = true//アニメーションを繰り返し行う
+        self.view.addSubview(animationView)
+        
+        
     }
+    
+    //アニメーション開始
+    @IBAction func startAct(_ sender: UIButton) {
+        print("アニメーション開始")
+        animationView.play()
+    }
+    
+
+    //アニメーションストップ
+    @IBAction func stopAct(_ sender: UIButton) {
+        print("アニメーション終了")
+        animationView.stop()
+    }
+    
+    
 
 
 }
